4619871d79129d6e7377ca6e7c252755631c3606
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-09-30 21:39:45 +0900

    플레이어 이동 및 충돌 구현

---
+++ Assets/Scripts/Player/PlayerContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerContact : MonoBehaviour
+{
+    [SerializeField] private LayerMask collisionLayer;
+    [SerializeField] private float checkRadius = 0.1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void CheckContact()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, checkRadius, 0);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject.CompareTag("Enemy"))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    private void Update()
+    {
+        
+    }
+}
