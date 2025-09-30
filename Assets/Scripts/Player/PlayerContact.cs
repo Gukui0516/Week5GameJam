c51f9a56be2625e60fc07299af3ac7330e7d3e07
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-09-30 21:54:40 +0900

    플레이어 충돌 처리

--- Assets/Scripts/Player/PlayerContact.cs
+++ Assets/Scripts/Player/PlayerContact.cs
@@ -2,22 +2,27 @@
 
 public class PlayerContact : MonoBehaviour
 {
-    [SerializeField] private LayerMask collisionLayer;
-    [SerializeField] private float checkRadius = 0.1f;
+    [SerializeField] CircleCollider2D checkRadius;
+    private void Awake()
+    {
+        
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void CheckContact()
     {
-        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, checkRadius, 0);
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, checkRadius.radius);
         foreach (Collider2D hit in hits)
         {
             if (hit.gameObject.CompareTag("Enemy"))
             {
+
+                Debug.Log("Contact Enemy");
                 gameObject.SetActive(false);
             }
         }
     }
     private void Update()
     {
-        
+        CheckContact();
     }
 }
